31a5642f0d580dd5846bf4b56d65e5f004c50a59
sainivas-yellapragada <sainivasyellapragada@gmail.com>
2024-11-21 11:37:54 +0530

    configured urls.py in app,voice_mail and added users to admin pannel

--- voice_mail/settings.py
+++ voice_mail/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s(g3te2g@bphbx-rx(k1hx#_%!r(4i7vozan7*q&vnwjh&h!kj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'voice_mail.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
